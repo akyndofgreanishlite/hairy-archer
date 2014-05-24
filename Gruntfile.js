a0b1af01a8521fd7235bbae5423fdceec8c461a5
akyndofgreanishlite <jameson.tyler@gmail.com>
2014-05-24 14:19:20 -0700

    Initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'static/css/main.css' : 'static/scss/main.scss'
+                }
+            }
+        },
+        watch: {
+            options: {
+                livereload: true,
+            },
+            html: {
+                files: ['index.html'],
+            },
+            js: {
+                files: ['js/**/*.js'],
+                tasks: ['jshint'],
+            },
+            css: {
+                files: ['scss/**/*.scss'],
+                tasks: ['sass'],
+            }
+        },
+        compress: {
+            main: {
+                // options: {
+                //     mode: 'gzip'
+                // },
+                expand: true,
+                cwd: 'static/',
+                src: ['**/*'],
+                // dest: 'public/'
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.registerTask('default',['sass','watch']);
+}
